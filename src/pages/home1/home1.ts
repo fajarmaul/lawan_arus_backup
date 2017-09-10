eac377484fdda4f78236e19d4dacfc878a38289a
unknown <ankarayu@gmail.com>
2017-09-10 22:57:45 +0700

    yayasan3

--- src/pages/home1/home1.ts
+++ src/pages/home1/home1.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Detail2Page } from '../detail2/detail2';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { DetailPage } from '../detail/detail';
+import { UploadPage } from "../upload/upload";
+import { FIREBASE_CONFIG } from "../../app/firebase.config";
+import { Camera, CameraOptions} from "@ionic-native/camera";
+import { storage, initializeApp} from 'firebase';
+
 /**
  * Generated class for the Home1Page page.
  *
@@ -15,16 +18,58 @@ import { AngularFireDatabase } from 'angularfire2/database';
   templateUrl: 'home1.html',
 })
 export class Home1Page {
-  yayasan: any;
+  image: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,  private fire: AngularFireAuth,
-              private firedata: AngularFireDatabase) {
-        const yayasan = this.firedata.list('/post_donatur/').subscribe(data =>{
-        console.log(data);
-        this.yayasan = data;
+  constructor(private camera: Camera, public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  async takePicture(){
+    try {
+      const options : CameraOptions = {
+        quality: 50, //to reduce img size
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL, //to make it base64 image
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType:this.camera.MediaType.PICTURE,
+        correctOrientation: true
+      }
+
+      const result =  await this.camera.getPicture(options);
+
+      this.image = 'data:image/jpeg;base64,' + result;
+
+      const picture = storage().ref('picture/photo/');
+      picture.putString(this.image, 'data_url');
+      
+
+    }
+    catch (e) {
+      console.error(e);
+      alert("error");
+    }
+
+  }
+
+  getPhotoFromGallery(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType     : this.camera.PictureSourceType.PHOTOLIBRARY,
+        targetWidth: 600,
+        targetHeight: 600
+    }).then((imageData) => {
+      // this.base64Image = imageData;
+      // this.uploadFoto();
+      this.image = 'data:image/jpeg;base64,' + imageData;
+
+      const picture = storage().ref('picture/photo/');
+      picture.putString(this.image, 'data_url');
+      
+            
+      }, (err) => {
     });
   }
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Home1Page');
@@ -32,11 +77,8 @@ export class Home1Page {
 
 /*  detail(){
 
-  //this.navCtrl.setRoot(DetailPage);
-    this.navCtrl.push(DetailPage);
+	//this.navCtrl.setRoot(DetailPage);
+		this.navCtrl.push(DetailPage);
 
-  }*/
-      itemTapped(data) {
-    this.navCtrl.push(Detail2Page, data);
-  }
+	}*/
 }
